814528196ef27021e9bf4558bea53bb00e58886f
prana <prakashrana9847067614@gmail.com>
2020-04-08 04:42:13 -0400

    [rana ]added age validations

--- app/src/main/java/com/example/c0773839_w2020_mad3125_midterm/PersonalDetailsFragment.java
+++ app/src/main/java/com/example/c0773839_w2020_mad3125_midterm/PersonalDetailsFragment.java
@@ -2,10 +2,12 @@ package com.example.c0773839_w2020_mad3125_midterm;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
@@ -17,12 +19,16 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.c0773839_w2020_mad3125_midterm.Model.DataSaver;
+import com.example.c0773839_w2020_mad3125_midterm.Util.Validation;
 import com.google.android.material.textfield.TextInputEditText;
 import com.stepstone.stepper.BlockingStep;
 import com.stepstone.stepper.StepperLayout;
 import com.stepstone.stepper.VerificationError;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
+import java.util.Date;
 
 
 /**
@@ -69,6 +75,7 @@ public class PersonalDetailsFragment extends Fragment  implements BlockingStep ,
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onNextClicked(final StepperLayout.OnNextClickedCallback callback) {
 
@@ -83,7 +90,20 @@ public class PersonalDetailsFragment extends Fragment  implements BlockingStep ,
         String dobString = dobEditText.getText().toString();
         if(dobString.trim().isEmpty()){
             dobEditText.setError("Date of Birth can't be empty");
+            return;
         }
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        LocalDate dateDob = LocalDate.parse(dobString,dateTimeFormatter);
+        if(!Validation.ageGreaterEqual(dateDob,18)){
+            dobEditText.setError("A person must be atleast 18 years old.");
+            return;
+        }
+
+        int selectedId = genderRadio.getCheckedRadioButtonId();
+        genderRadio =  findViewById(selectedId);
+
+
+
 
         callback.goToNextStep();
 
@@ -128,7 +148,16 @@ public class PersonalDetailsFragment extends Fragment  implements BlockingStep ,
 
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        String dateStr = year+"/"+month+"/"+dayOfMonth;
+        String month_str = Integer.toString(month);
+        if(month<10){
+            month_str = "0"+month_str;
+        }
+
+        String day_str = Integer.toString(dayOfMonth);
+        if(dayOfMonth<10){
+            day_str = "0"+day_str;
+        }
+        String dateStr = year+"/"+month_str+"/"+day_str;
         dobEditText.setText(dateStr);
     }
 }
