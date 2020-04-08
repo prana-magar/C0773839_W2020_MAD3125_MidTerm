932aa8e5bd409ede86f1013fc53a10250b54ba3b
prana <prakashrana9847067614@gmail.com>
2020-04-08 03:00:46 -0400

    [rana ] data saver object attached to fragmenttts

--- app/src/main/java/com/example/c0773839_w2020_mad3125_midterm/SinFragment.java
+++ app/src/main/java/com/example/c0773839_w2020_mad3125_midterm/SinFragment.java
@@ -1,5 +1,6 @@
 package com.example.c0773839_w2020_mad3125_midterm;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.c0773839_w2020_mad3125_midterm.Model.DataSaver;
 import com.stepstone.stepper.BlockingStep;
 import com.stepstone.stepper.StepperLayout;
 import com.stepstone.stepper.VerificationError;
@@ -22,6 +24,7 @@ import com.stepstone.stepper.VerificationError;
  */
 public class SinFragment extends Fragment  implements BlockingStep {
 
+    private DataSaver dataSaver;
     public SinFragment() {
         // Required empty public constructor
     }
@@ -64,5 +67,15 @@ public class SinFragment extends Fragment  implements BlockingStep {
     @Override
     public void onError(@NonNull VerificationError error) {
 
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof DataSaver) {
+            dataSaver = (DataSaver) context;
+        } else {
+            throw new IllegalStateException("Activity must implement DataManager interface!");
+        }
     }
 }
