08883a09d440b4f3902fcf4277414b9446c9c8b3
prana <prakashrana9847067614@gmail.com>
2020-04-08 02:22:53 -0400

    [rana ] made Personal details to implement Blocking step

--- app/src/main/java/com/example/c0773839_w2020_mad3125_midterm/PersonalDetailsFragment.java
+++ app/src/main/java/com/example/c0773839_w2020_mad3125_midterm/PersonalDetailsFragment.java
@@ -2,17 +2,23 @@ package com.example.c0773839_w2020_mad3125_midterm;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.stepstone.stepper.BlockingStep;
+import com.stepstone.stepper.StepperLayout;
+import com.stepstone.stepper.VerificationError;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PersonalDetailsFragment extends Fragment {
+public class PersonalDetailsFragment extends Fragment  implements BlockingStep {
 
     public PersonalDetailsFragment() {
         // Required empty public constructor
@@ -24,5 +30,36 @@ public class PersonalDetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_personal_details, container, false);
+    }
+
+    @Override
+    public void onNextClicked(StepperLayout.OnNextClickedCallback callback) {
+
+    }
+
+    @Override
+    public void onCompleteClicked(StepperLayout.OnCompleteClickedCallback callback) {
+
+    }
+
+    @Override
+    public void onBackClicked(StepperLayout.OnBackClickedCallback callback) {
+
+    }
+
+    @Nullable
+    @Override
+    public VerificationError verifyStep() {
+        return null;
+    }
+
+    @Override
+    public void onSelected() {
+
+    }
+
+    @Override
+    public void onError(@NonNull VerificationError error) {
+
     }
 }
