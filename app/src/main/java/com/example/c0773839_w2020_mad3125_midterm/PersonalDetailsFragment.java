c3e000878d3a597353268a8da173cb8675f0d590
prana <prakashrana9847067614@gmail.com>
2020-04-08 04:08:32 -0400

    [rana ] date picker for dob

--- app/src/main/java/com/example/c0773839_w2020_mad3125_midterm/PersonalDetailsFragment.java
+++ app/src/main/java/com/example/c0773839_w2020_mad3125_midterm/PersonalDetailsFragment.java
@@ -1,5 +1,6 @@
 package com.example.c0773839_w2020_mad3125_midterm;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 
@@ -11,21 +12,28 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.c0773839_w2020_mad3125_midterm.Model.DataSaver;
+import com.google.android.material.textfield.TextInputEditText;
 import com.stepstone.stepper.BlockingStep;
 import com.stepstone.stepper.StepperLayout;
 import com.stepstone.stepper.VerificationError;
+
+import java.util.Calendar;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PersonalDetailsFragment extends Fragment  implements BlockingStep {
+public class PersonalDetailsFragment extends Fragment  implements BlockingStep ,DatePickerDialog.OnDateSetListener{
 
 
     private DataSaver dataSaver;
+    TextInputEditText firstNameEditText, lastNameEditText, dobEditText;
+    RadioGroup genderRadio;
     public PersonalDetailsFragment() {
         // Required empty public constructor
     }
@@ -39,7 +47,38 @@ public class PersonalDetailsFragment extends Fragment  implements BlockingStep {
     }
 
     @Override
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        firstNameEditText = view.findViewById(R.id.FirstNameEditText);
+        lastNameEditText = view.findViewById(R.id.LastNameEditText);
+        dobEditText = view.findViewById(R.id.DOBEditText);
+        dobEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        v.getContext(), PersonalDetailsFragment.this,
+                        Calendar.getInstance().get(Calendar.YEAR),
+                        Calendar.getInstance().get(Calendar.MONTH),
+                        Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+                );
+                datePickerDialog.show();
+            }
+        });
+
+        genderRadio = view.findViewById(R.id.gender_radio);
+
+    }
+
+    @Override
     public void onNextClicked(final StepperLayout.OnNextClickedCallback callback) {
+
+        String firstName = firstNameEditText.getText().toString();
+        if(firstName.trim().isEmpty()){
+            firstNameEditText.setError("First Name can't be empty");
+            return;
+        }
+
+        String lastName = lastNameEditText.getText().toString();
 
 
         callback.goToNextStep();
@@ -81,5 +120,10 @@ public class PersonalDetailsFragment extends Fragment  implements BlockingStep {
         } else {
             throw new IllegalStateException("Activity must implement DataManager interface!");
         }
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
     }
 }
