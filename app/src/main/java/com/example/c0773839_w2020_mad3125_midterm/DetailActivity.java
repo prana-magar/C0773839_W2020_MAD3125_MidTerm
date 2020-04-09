b68b6bd288e49a296f8ef69fbe85b123919593c6
prana <prakashrana9847067614@gmail.com>
2020-04-09 11:26:23 -0400

    [rana ]detail page design

--- app/src/main/java/com/example/c0773839_w2020_mad3125_midterm/DetailActivity.java
+++ app/src/main/java/com/example/c0773839_w2020_mad3125_midterm/DetailActivity.java
@@ -3,12 +3,17 @@ package com.example.c0773839_w2020_mad3125_midterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
+    LinearLayout lr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+
     }
 }
